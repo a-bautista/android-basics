c8685b0075e9de4242e6444d7b5fd5426850defd
b9007 <alejandro.bautista90@yahoo.com.mx>
2016-08-30 18:37:48 -0500

    Home page and air temperature working

---
+++ app/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by abautista on 30/08/2016.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+
+ReactDOM.render(
+    routes,
+    document.getElementById('app')
+);
