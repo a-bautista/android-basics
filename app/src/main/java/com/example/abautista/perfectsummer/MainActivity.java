9606e80a1c455dfdeeafbb039e981a3d1e1678ae
b9007 <alejandro.bautista90@yahoo.com.mx>
2016-09-01 21:08:15 -0500

    data is almost inserted

--- app/src/main/java/com/example/abautista/perfectsummer/MainActivity.java
+++ app/src/main/java/com/example/abautista/perfectsummer/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.abautista.perfectsummer;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.EditText;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.database.Cursor;
+import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
     private static SeekBar seaWeatherSeekBar;
     private static TextView seaWeatherTextView;
 
+    Button showResults;
+    Button displayResults;
+
+    //create the database in the main activity
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         seeTheWeatherTemp();
         seeTheWeatherWater();
+
+        //this calls the constructor of the database
+        myDb = new DatabaseHelper(this);
+
+        showResults = (Button)findViewById(R.id.showResults);
+        viewResults();
+
+        displayResults = (Button)findViewById(R.id.displayResults);
+
     }
 
     public void seeTheWeatherTemp (){
@@ -68,9 +86,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
         );// End of the piece of code: see the current progress of the seekbar.
-
     }
-
 
     public void seeTheWeatherWater(){
 
@@ -109,13 +125,69 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void getValuesFromMainWindows(View view){
-
-
-
+    public void viewResults (){
+        showResults.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Cursor results = myDb.showResults();
+                        if (results.getCount() == 0) {
+                            showMessage("Error: ", "Nothing found.");
+                            return;
+                        } else {
+                            StringBuffer buffer = new StringBuffer();
+                            while (results.moveToNext()) {
+                                buffer.append("Code :" + results.getString(0) + "\n");
+                                buffer.append("City :" + results.getString(1) + "\n");
+                                buffer.append("Month :" + results.getString(2) + "\n\n");
+                            }
+                            showMessage("Data", buffer.toString());
+                        }
+                    }
+                }
+        );
     }
 
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
 
+    //not working
+    public void onClickDisplayResults(View view){
 
+        final Intent startNewActivity = new Intent(this,DisplayResults.class);
+        final Bundle bundle = new Bundle();
 
+        displayResults.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Cursor results = myDb.showResults();
+
+                        if (results.getCount() == 0) {
+                            showMessage("Error: ", "Nothing found.");
+                            return;
+
+                        } else {
+                            StringBuffer buffer = new StringBuffer();
+                            while (results.moveToNext()) {
+                                buffer.append("Id city :" + results.getString(0) + "\n");
+                                buffer.append("City :" + results.getString(1) + "\n\n");
+                            }
+                            bundle.putString("name",buffer.toString());
+                            startNewActivity.putExtras(bundle);
+
+                            //startNewActivity.putExtra(startNewActivity.EXTRA_TEXT, buffer.toString());
+                            //showMessage("Data", buffer.toString());
+                            startActivity(startNewActivity);
+                        }
+                    }
+                }
+        );
+    }
 }
